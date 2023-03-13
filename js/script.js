d81d2de2ac0a89d9c43598145226524680d5c5b7
marty_d <martydellorusso@gmail.com>
2023-03-13 15:01:29 -0400

    Created global variables

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Unordered list to show players guessed letters
+const guessedLettersElement = document.querySelector(".guessed-letters");
+// The button to submit a guess
+const guessLetterButton = document.querySelector(".guess");
+// Text input for player to guess a letter
+const letterInput = document.querySelector(".letter");
+// Word in progress paragraph
+const wordInProgress = document.querySelector(".word-in-progress");
+// Remaining guesses paragraph
+const remainingGuessesElement = document.querySelector(".remaining");
+// Span for the number of guesses
+const remainingGuessesSpan = document.querySelector(".remaining span");
+// Messages paragraph
+const message = document.querySelector(".message");
+// Hidden play again button
+const playAgainButton = document.querySelector(".play-again");
+// Test word
+const word = "magnolia";
